a4d9d9b644222e0e4ae730712a207c78ec424767
Taha Zabuawala <tzabuawala@gmail.com>
2014-11-07 09:50:06 +0530

    Update index.php

--- list/index.php
+++ list/index.php
@@ -205,7 +205,7 @@ function selectsingle(){
  <a id="checkcount">0</a> Message(s) selected
 
 <?php
-$spreadsheet_url="https://docs.google.com/spreadsheet/pub?key=0ArdhkPTPxvG2dG1mb2ZRbjVod1h2UDdvUzZKLWlyM3c&output=csv";
+$spreadsheet_url="https://docs.google.com/spreadsheets/d/1M_tPiZsAlsp9d_TwfE0k1C0ZlA0LiI7YYoFe1VJM7Bg/export?gid=0&format=csv";
 
 if(!ini_set('default_socket_timeout',15)) echo "<!-- unable to change socket timeout -->";
 
